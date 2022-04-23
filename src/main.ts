7e47c1f51e02bce6cc76ee169f68b66d55295349
Leonardo Pinto <leoviveiros@gmail.com>
2022-04-23 15:26:38 -0300

    impl api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,16 @@ async function bootstrap() {
 
     app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
 
+    const documentConfig = new DocumentBuilder()
+        .setTitle('NestJS API Example')
+        .setDescription('The API description')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, documentConfig);
+
+    SwaggerModule.setup('api', app, document);
+    
     await app.listen(3000);
 }
 bootstrap();
