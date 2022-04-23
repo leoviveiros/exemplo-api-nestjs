e84e7b84cbc99a8c091c78953b923a3a083f5e26
Leonardo Pinto <leoviveiros@gmail.com>
2022-04-23 14:00:58 -0300

    impl create and findall

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
 
-  findAll() {
-    return `This action returns all orders`;
-  }
+    constructor(@InjectModel(Order) private orderModel: typeof Order) { }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
+    create(createOrderDto: CreateOrderDto) {
+        this.orderModel.create({
+            amount: createOrderDto.amount,
+        });
+    }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
+    findAll() {
+        return this.orderModel.findAll();
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} order`;
+    }
+
+    update(id: number, updateOrderDto: UpdateOrderDto) {
+        return `This action updates a #${id} order`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} order`;
+    }
 }
