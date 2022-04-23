5217e4e86a0dad3183fe5a834bb5b86d7907287c
Leonardo Pinto <leoviveiros@gmail.com>
2022-04-23 13:20:19 -0300

    impl order model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Order } from './orders/entities/order.entity';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [OrdersModule],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'sqlite',
+            storage: './dist/db.sqlite',
+            autoLoadModels: true,
+            models: [Order]
+        }),
+        OrdersModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,11 @@
-export class Order {}
+import { Column, DataType, Model, PrimaryKey, Table } from 'sequelize-typescript';
+
+@Table({ tableName: 'orders' })
+export class Order extends Model {
+    @PrimaryKey
+    @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+    id: string;
+
+    @Column({ type: DataType.DECIMAL(10, 2), allowNull: false })
+    amount: number;
+}
